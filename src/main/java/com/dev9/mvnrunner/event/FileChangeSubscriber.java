7fad86ab9e7ccf35dcbd328b52a016b858475fea
Will Iverson <wiverson@gmail.com>
2015-06-26 16:29:00 -0700

    Added missing annotation
    Subscribe annotation needed for EventBus registration.

--- src/main/java/com/dev9/mvnrunner/event/FileChangeSubscriber.java
+++ src/main/java/com/dev9/mvnrunner/event/FileChangeSubscriber.java
@@ -1,16 +1,20 @@
 package com.dev9.mvnrunner.event;
 
+import com.google.common.eventbus.Subscribe;
+
 /**
  * Created by wiverson on 6/26/15.
  */
-public class FileChangeSubscriber implements  PathEventSubscriber {
+public class FileChangeSubscriber implements PathEventSubscriber {
+
+
+    @Subscribe
     @Override
     public void handlePathEvents(PathEventContext pathEventContext) {
 
         System.out.println("+");
 
-        for( PathEvent evt : pathEventContext.getEvents())
-        {
+        for (PathEvent evt : pathEventContext.getEvents()) {
             System.out.println("@>" + evt.getEventTarget().getFileName());
         }
     }
